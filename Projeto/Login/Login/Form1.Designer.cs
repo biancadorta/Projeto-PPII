6f3e65d2b1248c2f1d8e76c3046ab0fd9b91521d
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2018-04-10 08:41:55 -0300

    Upload dos arquivos

---
+++ Projeto/Login/Login/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Login
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.lCadastrar = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("OpenSymbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(233, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Senha:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("OpenSymbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(233, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.Location = new System.Drawing.Point(291, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 17);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.Location = new System.Drawing.Point(291, 81);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(140, 17);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.BackColor = System.Drawing.Color.White;
+            this.btnLogar.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogar.Location = new System.Drawing.Point(308, 137);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(87, 23);
+            this.btnLogar.TabIndex = 4;
+            this.btnLogar.Text = "Logar";
+            this.btnLogar.UseVisualStyleBackColor = false;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // lCadastrar
+            // 
+            this.lCadastrar.AutoSize = true;
+            this.lCadastrar.BackColor = System.Drawing.Color.Transparent;
+            this.lCadastrar.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lCadastrar.Location = new System.Drawing.Point(348, 109);
+            this.lCadastrar.Name = "lCadastrar";
+            this.lCadastrar.Size = new System.Drawing.Size(86, 16);
+            this.lCadastrar.TabIndex = 6;
+            this.lCadastrar.TabStop = true;
+            this.lCadastrar.Text = "Cadastrar-se";
+            this.lCadastrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lCadastrar_LinkClicked);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Login.Properties.Resources.planetinhasinhas;
+            this.ClientSize = new System.Drawing.Size(716, 366);
+            this.Controls.Add(this.lCadastrar);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("OpenSymbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmLogin";
+            this.Text = "Sun System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogar;
+        private System.Windows.Forms.LinkLabel lCadastrar;
+    }
+}
+
---
+++ Projeto/Login/Login/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login
+{
+    public partial class frmLogin : Form
+    {
+        String cs = Properties.Settings.Default.ConnectionString;
+        frmCadastro cadastro;
+        public frmLogin()
+        {
+            InitializeComponent();
+            cadastro = new frmCadastro();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSenha.PasswordChar = '*';
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lCadastrar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            cadastro.Show();
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text.Equals("") || txtSenha.Text.Equals(""))
+            {
+                MessageBox.Show("Por favor, preencha os campos corretamente.");
+            }
+            else {
+                try
+                {
+                    SqlConnection con = new SqlConnection();
+                    con.ConnectionString = cs;
+
+                    String consulta = "Select Usuario where email = @email and  senha = @senha";
+                    SqlCommand cmd = new SqlCommand(consulta,con); //Representa um comando ou procedimento que sera feito bd
+                    cmd.Parameters.AddWithValue("@email", txtName.Text);
+                    cmd.Parameters.AddWithValue("@senha",txtName.Text);
+
+                    SqlDataAdapter data = new SqlDataAdapter(); 
+                    
+
+                }
+                catch (Exception erro)
+                {
+                    MessageBox.Show("Erro:" + erro.ToString());
+                }
+            }
+        }
+    }
+}
